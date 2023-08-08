7a46221b01a36081dcf5bcab69d8c5719b263e0d
Yangdoubleh <why374@naver.com>
2023-08-08 15:58:18 +0900

    Feat(#10-jasypt) : Jasypt 적용하여 yml 파일 암호화.

--- back/configserver/src/test/java/malkongpudding/configserver/ConfigserverApplicationTests.java
+++ back/configserver/src/test/java/malkongpudding/configserver/ConfigserverApplicationTests.java
@@ -1,6 +1,7 @@
 package malkongpudding.configserver;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.condition.EnabledIfEnvironmentVariable;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
